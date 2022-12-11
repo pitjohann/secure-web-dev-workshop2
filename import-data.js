0f09adf645b95b7c47fd802cbcbf43d6123a1b28
pitjohann <johann.maillard@edu.devinci.fr>
2022-12-11 11:25:45 +0100

    All besides update

--- import-data.js
+++ import-data.js
@@ -35,7 +35,7 @@ function loadFilmingLocations(Locations){
     const filmingLocations = require('./lieux-de-tournage-a-paris.json')
 
     for(const element of filmingLocations){
-        const locat = new Locations({
+        const locat = new Location({
             "filmType":  element.fields.type_tournage,
             "filmProducerName" : element.fields.nom_producteur,
             "endDate" : new Date(element.fields.date_fin),
@@ -54,24 +54,45 @@ function loadFilmingLocations(Locations){
     }
 }
 
-function findOneById(Locations, locationId){
-    const filmingLocations = require('./lieux-de-tournage-a-paris.json')
 
-    Location.findOne({sourcelLocationId : locationId})
+
+function queryLocationById(id) {
+    Location.findOne({sourceLocationId: id}).then(film => console.log(film+"\n"))
+}
+function queryAllByName(nomTournage) {
+    Location.find({filmName : nomTournage}).then(film => console.log(film))
+}
+function queryIdandDelete(id){
+    Location.findOneAndDelete({sourceLocationId : id}).then(film=>console.log("deleted filmLocation"))
 }
 
-function queryLocationById(idInMongo) {
-    Location.findOne({sourceLocationId: idInMongo}).then(film => console.log(film+"\n"))
-}
-function queryAll() {
-    Location.find({}).then(film => console.log(film))
+function addALocation(newLoc){
+    newLoc.save()
+    console.log("New Location Added!")
 }
 async function main(){
 
     await mongoose.connect(process.env.MONGO_URI)
     //await loadFilmingLocations(Locations)
     //await queryLocationById('2020-412')
-    await queryAll()
+    //await queryAllByName("Une jeune fille qui va bien")
+    //await queryIdandDelete('2020-412')
+
+    const newLoc = new Location({
+        "filmType":  "Horror",
+        "filmProducerName" : "MOREAU Roch",
+        "endDate" : "2020-01-01",
+        "filmName" : "A day in a Web Class",
+        "district" : "75001",
+        "geolocation":{"coordinates": [2.348314535961912, 48.83566000015182], "type": "Point"},
+        "sourceLocationId":'2020-05-07',
+        "filmDirectorName":"ROCH Moreau",
+        "adress": "Somewhere in Paris",
+        "startDate":"2019-01-01",
+        "year": parseInt("2015")
+
+    })
+    await addALocation(newLoc)
     console.log("Found Location")
 }
 
